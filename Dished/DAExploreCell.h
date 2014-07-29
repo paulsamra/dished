bc436353be8b246ccd742572c2e426bb0d6fa6d8
POST <dstimm@Daryls-MacBook-Pro.local>
2014-07-29 10:57:34 -0700

    added DAExploreCell and DAExploreViewController added a UIContainerview with custom cells and a tableview with search bar.  The search bar is not connected to anything.  Known bugs, the collection view is offset and is scrolled down too far.  Also the spacing between cells need to be slightly adjusted, and the fonts need to be changed to the fonts matching the mockup.

---
+++ Dished/DAExploreCell.h
@@ -0,0 +1,15 @@
+//
+//  DAExploreCell.h
+//  Dished
+//
+//  Created by POST on 7/28/14.
+//  Copyright (c) 2014 Dished. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface DAExploreCell : UICollectionViewCell
+@property (weak, nonatomic) IBOutlet UIImageView *image;
+@property (weak, nonatomic) IBOutlet UILabel *hashLabel;
+
+@end
--- Dished/View Controllers/DAExploreViewController.m
+++ Dished/View Controllers/DAExploreViewController.m
@@ -9,9 +9,11 @@
 #import "DAExploreViewController.h"
 #import "DAAPIManager.h"
 #import "DANegativeHashtagsViewController.h"
-
+#import "DAExploreCell.h"
 
 @interface DAExploreViewController()
+@property (weak, nonatomic) IBOutlet UICollectionView *exploreCollectionView;
+@property (weak, nonatomic) IBOutlet UITableView *tableView;
 
 @end
 
@@ -21,6 +23,66 @@
 - (void)viewDidLoad
 {
     [super viewDidLoad];
+    
 }
 
+
+- (NSInteger)numberOfSectionsInTableView:(UITableView *)tableView
+{
+    return 1;
+}
+
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section
+{
+	return 2;
+}
+
+
+- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    
+    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:@"cell" forIndexPath:indexPath];
+    if (indexPath.row == 0) {
+        
+        cell.textLabel.text = @"Editor's Picks";
+
+    } else {
+        cell.textLabel.text = @"Popular Now";
+
+    }
+
+    return cell;
+}
+
+
+
+
+- (NSInteger)collectionView:(UICollectionView *)view numberOfItemsInSection:(NSInteger)section {
+    return 12;
+}
+
+- (NSInteger)numberOfSectionsInCollectionView: (UICollectionView *)collectionView {
+    return 1;
+}
+
+- (UICollectionViewCell *)collectionView:(UICollectionView *)cv cellForItemAtIndexPath:(NSIndexPath *)indexPath {
+    DAExploreCell *cell = [cv dequeueReusableCellWithReuseIdentifier:@"cell" forIndexPath:indexPath];
+    cell.backgroundColor = [UIColor whiteColor];
+
+    cell.image.image = [UIImage imageNamed:@"food.jpg"];
+
+    return cell;
+}
+
+
+- (CGSize)collectionView:(UICollectionView *)collectionView layout:(UICollectionViewLayout*)collectionViewLayout sizeForItemAtIndexPath:(NSIndexPath *)indexPath {
+
+    return CGSizeMake((self.view.frame.size.width / 3)-4, (self.view.frame.size.width / 4)-4);
+}
+
+
+//- (UIEdgeInsets)collectionView:
+//(UICollectionView *)collectionView layout:(UICollectionViewLayout*)collectionViewLayout insetForSectionAtIndex:(NSInteger)section {
+//    return UIEdgeInsetsMake(20, 20, 50, 20);
+//}
 @end
